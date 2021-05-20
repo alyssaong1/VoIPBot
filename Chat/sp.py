b50cbfb8bb8ceefc4ee317a78b7b7931383494a0
Pratyush Kumar Jena <pratyush.jena@sravathi.co.in>
2021-05-20 22:47:12 +0530

    intention detection

---
+++ Chat/sp.py
@@ -0,0 +1,11 @@
+import azure.cognitiveservices.speech as speechsdk
+
+def from_file():
+    speech_config = speechsdk.SpeechConfig(subscription="<paste-your-subscription-key>", region="<paste-your-region>")
+    audio_input = speechsdk.AudioConfig(filename="your_file_name.wav")
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    
+    result = speech_recognizer.recognize_once_async().get()
+    print(result.text)
+
+from_file()
