a0a77a3beb50cd6fe8218df93639965ba8f7c866
Pratyush jena <37341004+pratyushjena2k17@users.noreply.github.com>
2021-05-20 23:11:55 +0530

    Add files via upload

---
+++ sp.py
@@ -0,0 +1,22 @@
+import azure.cognitiveservices.speech as speechsdk
+
+def from_file():
+    speech_config = speechsdk.SpeechConfig(subscription="34c15a8a2f4342b487ce48da2e8bdd12", region="eastus")
+    audio_input = speechsdk.AudioConfig(filename="output.wav")
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+    
+    result = speech_recognizer.recognize_once_async().get()
+    print(result.text)
+
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(result.text))
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+
+
+from_file()
